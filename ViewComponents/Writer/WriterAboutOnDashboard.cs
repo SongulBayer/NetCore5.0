d150d823612cf99e1d3c7c1b0894bde54e8513e2
snglbyrr8 <63016233+snglbyrr8@users.noreply.github.com>
2022-07-27 00:05:34 +0300

    Dashboard Added

---
+++ ViewComponents/Writer/WriterAboutOnDashboard.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrate;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCore5._0.ViewComponents.Writer
+{
+    public class WriterAboutOnDashboard:ViewComponent
+    {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+        public IViewComponentResult Invoke()
+        {
+            var val = wm.GetWriterById(8);
+            return View(val);
+        }
+
+    }
+}
