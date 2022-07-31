4f15d4e741c66d78dd2e640432ca935ee4f6eb08
snglbyrr8 <63016233+snglbyrr8@users.noreply.github.com>
2022-08-01 01:40:32 +0300

    Added Two Foreıgn Keys From Same Table

---
+++ Controllers/Message2Controller.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLayer.Concrate;
+using DataAccessLayer.Concrate;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCore5._0.Controllers
+{ 
+    [AllowAnonymous]
+    public class Message2Controller : Controller
+    {
+       
+        Message2Manager mm = new Message2Manager(new EfMessage2Repository());
+        public IActionResult GetAllMessages()
+        {
+            int id = 9;
+            var val = mm.getInboxListByWriter(id);
+            return View(val);
+        }
+        public IActionResult MessageDetails(int id)
+        {
+            Context c = new Context();
+            
+            var val = mm.TgetByid(id);
+            return View(val);
+        }
+    }
+}
