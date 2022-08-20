f62ded74537a8501b7ac738d2fb4424a1bc323a1
SongulBayer <sngll.byrr8@gmail.com>
2022-08-20 21:34:54 +0300

    Identity AppUser

---
+++ Controllers/RegisterUserController.cs
@@ -0,0 +1,55 @@
+﻿using EntityLayer.Concrate;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NetCore5._0.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCore5._0.Controllers
+{
+    [AllowAnonymous]
+    public class RegisterUserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterUserController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserSignUpViewModel p)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser()
+                {
+                    Email = p.Mail,
+                    NameSurname = p.NameSurname,
+                    UserName = p.UserName
+                };
+                var result = await _userManager.CreateAsync(appUser, p.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(p);
+        }
+    }
+}
---
+++ Models/UserSignUpViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCore5._0.Models
+{
+    public class UserSignUpViewModel
+    {
+        [Display(Name = "İsim Soyisim")]
+        [Required(ErrorMessage = "Gerekli")]
+        public string NameSurname { get; set; }
+
+        [Display(Name = "Şifre")]
+        [Required(ErrorMessage = "Gerekli")]
+        public string Password { get; set; }
+
+        [Display(Name = "Şifre Tekrar")]
+        [Required(ErrorMessage = "Gerekli")]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = "Kullanıcı Adı")]
+        [Required(ErrorMessage = "Gerekli")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Mail")]
+        [Required(ErrorMessage = "Gerekli")]
+        public string Mail { get; set; }
+
+    }
+}
